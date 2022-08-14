b7025f1f16002ae20ebf6d798e948ea52bff40a5
zainab <zainab@gmail.com>
2022-08-14 06:37:24 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+
+let footer = document.querySelector('.footer')
+console.log('footer')
+footer.innerHTML = `This site is developed with <i class="fa fa-heart" style="font-size:15px;color:red"></i>
+ by Zainab Alshingiti © 2022`
